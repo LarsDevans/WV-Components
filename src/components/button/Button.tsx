35ce2cbad9d5b0c8589ef1d0da37150486aa23c2
Lars Devans <l.beijaard1@student.avans.nl>
2025-04-05 13:13:16 +0200

    Add Framer Motion and Lucide React dependencies

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,11 +1,10 @@
-interface ButtonProps {
-  label: string;
-}
+import { motion } from 'framer-motion';
+import { ButtonProps } from '../../types/interactables';
 
 export const Button = ({ label }: ButtonProps) => {
   return (
-    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+    <motion.button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
       {label}
-    </button>
+    </motion.button>
   );
 };
